1282bd2509d6b2f88fb7123749f5b3f79c732624
Omar Elshimi <70725794+Omarelshimi@users.noreply.github.com>
2021-06-11 13:46:14 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,21 +3,37 @@
 // Initialize canvas and graphics context
 initGraphics("my-canvas", 600, 600);
 
-snake = [
-        {x: 650, y:500},
-        {x: 645, y:500},
-        {x: 640, y:500},
-        {x: 635, y:500},
-        {x: 630, y:500},
-        {x: 625, y:500},
-];
+snake = [];
+
+function snakeObject() {
+    return {
+        x: 250,
+        y: 200
+    }
+}
+
+for (let i = 0; i < 8; i++) {
+    snake.push(snakeObject());
+    snake[i].x += 10;
+}
+
+// requestAnimationFrame(draw);
+
+// function draw() {
+//     for (let i = 0; i < snake.length; i++) {
+
+//     }
+//     requestAnimationFrame(draw);
+// }
 
 function drawSnakePart(aSnake) {
     fill("green");
     circle(aSnake.x, aSnake.y, 10, "fill");
-} 
+}
 function drawSnake() {
-    snake.forEach(drawSnakePart);
+    for (let i = 0; i < snake.length; snake++) {
+        drawSnakePart(snake[i]);
+    }
 }
 
-
+drawSnake();
