bce4007fb5eb585d34667b989e113de661da954a
Omar Elshimi <70725794+Omarelshimi@users.noreply.github.com>
2021-06-14 14:14:35 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,36 +4,52 @@
 initGraphics("my-canvas", 600, 600);
 
 snake = [];
+const speed = 5;
+
+for (let i = 0; i < 8; i++) {
+    snake.push(snakeObject());
+    snake[i].x -= i * 5;
+}
 
 function snakeObject() {
     return {
         x: 250,
         y: 200
-    }
+    };
 }
 
-for (let i = 0; i < 8; i++) {
-    snake.push(snakeObject());
-    snake[i].x += 10;
-}
-
-// requestAnimationFrame(draw);
-
-// function draw() {
-//     for (let i = 0; i < snake.length; i++) {
-
-//     }
-//     requestAnimationFrame(draw);
-// }
 
 function drawSnakePart(aSnake) {
     fill("green");
     circle(aSnake.x, aSnake.y, 10, "fill");
 }
+
+let framecount = 0;
+
+function moveSnake(event) {
+    if (event.keyCode === 13) {
+        if (framecount % 60 === 0) {
+            snake.splice(snake.length - 1, 1)
+            snake.unshift(snakeObject());
+            snake[0].x = snake[1].x + speed;
+        }
+    }
+}
+
 function drawSnake() {
     for (let i = 0; i < snake.length; i++) {
         drawSnakePart(snake[i]);
     }
 }
 
-drawSnake();
+requestAnimationFrame(draw);
+
+function draw() {
+    background("lightsalmon");
+    for (let i = 0; i < snake.length; i++) {
+        drawSnake();
+    }
+    moveSnake();
+    requestAnimationFrame(draw);
+}
+
